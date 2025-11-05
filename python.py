dcbddc77c6194aea5f780e7853f8c087fe33ceda
Govind Charpe <govind.charpe16@gmail.com>
2025-11-05 17:58:57 +0000

    added some issues in python.py

--- python.py
+++ python.py
@@ -14,3 +14,26 @@ def calculator(a,b,c):
     else:
         return "Unsupported operator"
 print(calculator(2,4,"-"))
+
+class bankAccount:
+    __accountNumber = None
+    __accountHolder = None
+    __balance = None
+    def __init__(self, accountNumber, accountHolder):
+        self.__accountNumber = _____________
+        self.__accountHolder = _____________
+        self.__balance = _
+    def deposited(self, _____):
+        bankAccount.__balance += ______
+        print(f"Deposited: ${bankAccount.__balance}")
+    def withdrawl(self, _____):
+        bankAccount.__balance -= ______
+        print(f"Deposited: ${bankAccount.__balance}")
+    def getAccountInfo(self):
+        print(f"Account Number: {__________}")
+        print(f"Account Holder: {}")
+        print(f"Balance: {}")
+bankObj = bankAccount(12212, _____)
+bankObj._____(1000)
+bankObj._____(500)
+bankObj.getAccountInfo()
